ad887008eb9ebbe5aefa07bb87c2590c0298094e
bcurby <41361395+bcurby@users.noreply.github.com>
2020-08-04 14:48:12 +1000

    Initial commit of stripe server

---
+++ index.js
@@ -0,0 +1,36 @@
+//const functions = require('firebase-functions');
+
+const express = require("express");
+const app = express();
+const { resolve } = require("path");
+// This is your real test secret API key.
+const stripe = require("stripe")("sk_test_51HBYC3D60uGVgHNv1uDUE8FbPZUbjKgqGzEeh31yuTH8kM2PKM6UKyEbHrVrquNMeB4SqyfDPqBPxLpzCOQbVtVZ001oxtGVYU"); //Your secret key from stripe
+app.use(express.static("."));
+app.use(express.json());
+const calculateOrderAmount = items => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  console.log(items[0].amount)
+  return items[0].amount;
+};
+app.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+  const { currency } = req.body;
+  console.log(currency)
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: currency
+  });
+  res.send({
+    clientSecret: paymentIntent.client_secret
+  });
+});
+//for test whether app is working or not
+app.get('/greet',(req,res)=>{
+  res.send("It is working fine")
+})
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log('Node server listening on port ${PORT}'));
